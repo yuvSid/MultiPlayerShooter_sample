914d9d676256f21ff9dec122f26f05d97948007a
Mr Ego Brain <syvwgl@gmail.com>
2020-01-30 16:42:50 +0300

    End of first part tutorial

---
+++ Source/MultiPlayerShooter/MultiPlayerShooterGameModeBase.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+
+#include "MultiPlayerShooterGameModeBase.h"
+
+void AMultiPlayerShooterGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	if ( GEngine )
+	{
+		// Display a debug message for five seconds. 
+		// The -1 "Key" value (first argument) indicates that we will never need to update or refresh this message.
+		GEngine->AddOnScreenDebugMessage( -1, 5.0f, FColor::Yellow, TEXT( "Hello World, this is FPSGameMode!" ) );
+	}
+}
